f95b6b82cbac6befcf45c9bdcc0aeba9b91e4f8d
purplena <=>
2024-06-07 09:36:31 +0200

    project 18

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,16 @@
-console.log('numbers project');
+const updateCount = (element) => {
+  let counter = 0;
+  const increaseCount = setInterval(() => {
+    const value = element.dataset.value;
+    const increment = Math.ceil(value / 1000);
+    counter += increment;
+    element.textContent = `${counter}+`;
+    if (counter >= value) {
+      clearInterval(increaseCount);
+    }
+  }, 1);
+};
+
+Array.from(document.querySelectorAll('.number'), (span) => {
+  updateCount(span);
+});
